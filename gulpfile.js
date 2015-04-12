27462e24b6253995051bc4b631a18c059d3c3302
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-04-12 13:54:27 +0800

    Support jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 'use strict';
 
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+
+gulp.task('jshint', function () {
+    return gulp.src([
+                    'gulpfile.js',
+                ])
+               .pipe(jshint('.jshintrc'))
+               .pipe(jshint.reporter('jshint-stylish'));
+});
 
 gulp.task('test', function () {
     return gulp.src('package.json', {read: false})
